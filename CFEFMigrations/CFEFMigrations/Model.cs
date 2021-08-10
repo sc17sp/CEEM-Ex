2cf9035bbfb9ed4c188b2c87756f7a31c1114a35
sc17sp <sean.parker@x-labsystems.co.uk>
2021-08-10 16:50:49 +0100

    Cretaed Blog class and context

---
+++ CFEFMigrations/CFEFMigrations/Model.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity.Infrastructure;
+
+namespace CFEFMigrations
+{
+    public class BlogContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+    }
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+    }
+}
