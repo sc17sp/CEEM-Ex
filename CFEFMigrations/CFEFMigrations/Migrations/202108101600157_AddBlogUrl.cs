f7009f09085d074d534791823da8ae281f4ba5ef
sc17sp <sean.parker@x-labsystems.co.uk>
2021-08-10 17:05:23 +0100

    code migrations for new Blog URL

---
+++ CFEFMigrations/CFEFMigrations/Migrations/202108101600157_AddBlogUrl.cs
@@ -0,0 +1,18 @@
+﻿namespace CFEFMigrations.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddBlogUrl : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Blogs", "url", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Blogs", "url");
+        }
+    }
+}
---
+++ CFEFMigrations/CFEFMigrations/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace CFEFMigrations.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CFEFMigrations.BlogContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "CFEFMigrations.BlogContext";
+        }
+
+        protected override void Seed(CFEFMigrations.BlogContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
